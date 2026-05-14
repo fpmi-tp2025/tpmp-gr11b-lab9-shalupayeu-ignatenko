1459cd9bd4ab879b9cb014fb0fa92646c94857ff
andreishal <andreishal@icloud.com>
2026-05-14 19:39:55 +0300

    Initial Commit

---
+++ lab9/lab9App.swift
@@ -0,0 +1,17 @@
+//
+//  lab9App.swift
+//  lab9
+//
+//  Created by Курч Никита on 14.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct lab9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
